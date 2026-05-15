7f08a53477297da599c0a7cac13f9c746077c796
snehal10-jadhav <snehal10.jadhav@gmail.com>
2026-05-15 13:03:15 -0400

    Add tests to verify login on chrome browser

---
+++ contest.py
@@ -0,0 +1,20 @@
+import  pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action="store", default="chrome", help="my option: type1 or type2"
+    )
+
+@pytest.fixture
+def context(playwright, request):
+    browser_name = request.config.getoption("--browser_name")
+
+    if browser_name == "chrome":
+        browser = playwright.chromium.launch(headless=False)
+    else:
+        browser = playwright.firefox.launch(headless=False)
+    context = browser.new_context()
+
+    yield context
+    browser.close()
